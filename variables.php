940520bd36bdff18af721d31f004bb17b4388445
Franck1981-dev <franck.bouchet1981@gmail.com>
2021-03-16 11:14:03 +0100

    First issue

---
+++ variables.php
@@ -0,0 +1,16 @@
+<?php
+
+$nameMovie = "Indiana Jones and the Last Crusade";
+// $nameMovie est déclaré
+echo $nameMovie."\n";
+
+$alreadySeen = true;
+if (is_bool($alreadySeen) === true) {
+    echo "Oui "."\n";
+}
+
+$releaseYear = 1989;
+echo $releaseYear."\n";
+
+$note= "8.2/10 ";
+echo $note;
